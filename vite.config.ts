5515087b863ec8789cf69beddaaff23cd69c4746
Yap Jia Hong <yapjiahong-p@proton.me>
2025-07-06 18:18:26 +0800

    feat(https): added https

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import fs from "fs";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    https: {
+      key: fs.readFileSync("localhost-key.pem"),
+      cert: fs.readFileSync("localhost.pem"),
+    },
+  },
 });
