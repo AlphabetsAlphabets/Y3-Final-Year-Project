1dc485c623e300b6c2d3f7739492a6fd7b7b8a2c
Yap Jia Hong <yapjiahong-p@proton.me>
2025-07-12 17:01:32 +0800

    wip: sqlite it is more usable now

---
+++ src/lib/types/promiser.ts
@@ -0,0 +1,46 @@
+export type Promiser = {
+  (messageType: "open", args: OpenArgs): Promise<OpenResponse>;
+  (
+    messageType: "exec",
+    args: ExecArgs & { rowMode: "array" },
+  ): Promise<RowModeArray>;
+  (
+    messageType: "exec",
+    args: ExecArgs & { rowMode: "object" },
+  ): Promise<object[]>;
+  (messageType: "exec", args: ExecArgs): Promise<RowModeObject>;
+};
+
+type OpenArgs = {
+  filename: string;
+  vfs: string;
+};
+
+type OpenResponse = {
+  messageId: string;
+  result: {
+    filename: string;
+    dbId: string;
+    persistent: boolean;
+    vfs: string;
+  };
+};
+
+type ExecArgs = {
+  sql: string | (() => void);
+  bind?: string[];
+  callback?: (statement: string) => void;
+  rowMode?: "array" | "object" | "stmt";
+};
+
+export type RowModeArray = {
+  dbId: string;
+  messageId: string;
+  result: { resultRows: object[] };
+};
+
+export type RowModeObject = {
+  dbId: string;
+  messageId: string;
+  result: { sql: string; rowMode: string; resultRows: object[] };
+};
--- src/routes/database/+page.svelte
+++ src/routes/database/+page.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
     import { onMount } from "svelte";
 
-    let syncWorker: Worker | undefined = undefined;
-
     const loadWorker = async () => {
-        const SyncWorker = await import("$lib/workers/database.worker?worker");
+        const Worker = await import("$lib/workers/database.worker?worker");
+        let worker = new Worker.default();
 
-        syncWorker = new SyncWorker.default();
-
-        syncWorker.onmessage = (e) => {
+        worker.onmessage = (e) => {
             console.log("message received from worker.");
             console.log(e.data);
         };
 
-        syncWorker.postMessage("Hello from +page.svelte");
+        worker.postMessage({ command: "schema", messageId: 1 });
+        worker.postMessage({ command: "reset", messageId: 2 });
+        worker.postMessage({ command: "list", messageId: 3 });
     };
 
     onMount(loadWorker);
