21333beb6d537f488c865c3f26ba64c375270ae1
Yap Jia Hong <yapjiahong-p@proton.me>
2025-07-22 20:47:19 +0800

    fix(header): header already set error
    no clue if this will fix it but I sure hope so.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,10 +5,3 @@
 
 export const prerender = true;
 export const trailingSlash = "always";
-
-export function load({ setHeaders }) {
-  setHeaders({
-    "Cross-Origin-Embedder-Policy": "require-corp",
-    "Cross-Origin-Opener-Policy": "same-origin",
-  });
-}
