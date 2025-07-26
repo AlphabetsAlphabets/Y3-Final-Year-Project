c274a6f3f899d63ddee85d18917edec424391231
Yap Jia Hong <yapjiahong-p@proton.me>
2025-07-26 15:04:05 +0800

    'fixed' type issues.

--- src/lib/workers/database.worker.ts
+++ src/lib/workers/database.worker.ts
@@ -26,6 +26,7 @@ const dbWorker = {
     const list: Activity[] = [];
     if (response && response.result && response.result.resultRows) {
       response.result.resultRows.forEach((value) => {
+        // @ts-expect-error This will always be Activity
         list.push({ id: value[0], name: value[1] });
       });
     }
@@ -35,17 +36,7 @@ const dbWorker = {
 
   async addActivity(name: string): Promise<Activity[]> {
     await this.insert("activity", "name", `'${name}'`);
-    const response = await this.list("activity");
-
-    // Clear and update names array
-    const list: Activity[] = [];
-    if (response && response.result && response.result.resultRows) {
-      response.result.resultRows.forEach((value) => {
-        list.push({ id: value[0], name: value[1] });
-      });
-    }
-
-    return list;
+    return await this.listActivities();
   },
 
   async listProjects(): Promise<Project[]> {
@@ -54,6 +45,7 @@ const dbWorker = {
     const list: Project[] = [];
     if (response && response.result && response.result.resultRows) {
       response.result.resultRows.forEach((value) => {
+        // @ts-expect-error This will always be Project
         list.push({ name: value[0], color: value[1] });
       });
     }
@@ -63,17 +55,7 @@ const dbWorker = {
 
   async addProject(name: string, color: string): Promise<Project[]> {
     await this.insert("project", "name, color", `'${name}', '${color}'`);
-    const response = await this.list("project");
-
-    // Clear and update names array
-    const list: Project[] = [];
-    if (response && response.result && response.result.resultRows) {
-      response.result.resultRows.forEach((value) => {
-        list.push({ name: value[0], color: value[1] });
-      });
-    }
-
-    return list;
+    return await this.listProjects();
   },
 };
 
