6f37122506371b191072b80ee21ddaf23f520ae0
Yap Jia Hong <yapjiahong-p@proton.me>
2025-07-31 10:32:55 +0800

    forgot to remove https

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import fs from "fs";
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
-    https: {
-      key: fs.readFileSync("192.168.100.181+3-key.pem"),
-      cert: fs.readFileSync("192.168.100.181+3.pem"),
-    },
+    // https: {
+    //   key: fs.readFileSync("192.168.100.181+3-key.pem"),
+    //   cert: fs.readFileSync("192.168.100.181+3.pem"),
+    // },
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
