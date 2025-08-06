76355a7a1546e2b1ed0733f1f832070488c41898
Yap Jia Hong <yapjiahong-p@proton.me>
2025-08-06 09:10:09 +0800

    wip: update log in calendar

--- src/lib/components/Calendar.svelte
+++ src/lib/components/Calendar.svelte
@@ -2,12 +2,30 @@
     import type { CalendarEvent } from "$lib/calendar";
     import { Calendar, Interaction, TimeGrid } from "@event-calendar/core";
 
+    import Modal from "./Modal.svelte";
+    import ContextMenuModal from "./derived/ContextMenuModal.svelte";
+
     let { events }: { events: CalendarEvent[] } = $props();
+
+    let modal: Modal | null = $state(null);
+    let message = $state("");
 
     let options = $state({
         view: "timeGridWeek",
         events: events,
+        eventDidMount: function (arg) {
+            arg.el.addEventListener("click", () => {
+                message = "Goodbye";
+                modal?.showModal();
+            });
+        },
     });
 </script>
 
+<ContextMenuModal
+    bind:modal
+    title="Edit event"
+    {message}
+    type="message-modal-ok"
+/>
 <Calendar plugins={[TimeGrid, Interaction]} {options} />
---
+++ src/lib/components/derived/ContextMenuModal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Modal from "../Modal.svelte";
+    let { modal = $bindable(), title, message, type } = $props();
+</script>
+
+<div class={type}>
+    <Modal bind:this={modal} {title}>
+        <p>Title</p>
+        <input value={title} />
+        <!-- Turn this into a drop down so users can update the calendar information. -->
+        <button>Confirm</button>
+    </Modal>
+</div>
+
+<style>
+    .message-modal-ok :global(.modal-content) {
+        background-color: blue;
+        color: white;
+        width: 75vw; /* Sets width to 25% of the viewport width */
+        min-width: 0; /* Overrides the base min-width */
+    }
+
+    /* --- Responsive Styles for Small Screens --- */
+    @media (max-width: 768px) {
+        .message-modal-wrapper :global(.modal-content) {
+            width: 90vw; /* Use 90% of the viewport width on smaller screens */
+        }
+    }
+</style>
