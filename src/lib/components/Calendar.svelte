b2354fa2154fb768715776de56baaa09b6a7381b
Yap Jia Hong <yapjiahong-p@proton.me>
2025-08-12 21:09:15 +0800

    feat: #44

--- src/lib/components/Calendar.svelte
+++ src/lib/components/Calendar.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import type { CalendarEvent } from "$lib/calendar";
     import { Calendar, Interaction, TimeGrid } from "@event-calendar/core";
-    import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher, onMount } from "svelte";
+    import * as Comlink from "comlink";
+    import type { DbWorker } from "$lib/workers/database.worker";
 
     import Modal from "./Modal.svelte";
     import ContextMenuModal from "./derived/ContextMenuModal.svelte";
@@ -9,25 +11,54 @@
     let { events }: { events: CalendarEvent[] } = $props();
 
     let modal: Modal | null = $state(null);
-    let event: CalendarEvent | undefined = $state();
+    let targetEvent: CalendarEvent | undefined = $state();
     const dispatch = createEventDispatcher();
+    let dbWorker: Comlink.Remote<DbWorker> | null = $state(null);
+
+    const loadWorker = async () => {
+        const Worker = await import("$lib/workers/database.worker?worker");
+        dbWorker = Comlink.wrap<DbWorker>(new Worker.default());
+        await dbWorker.initWorker();
+    };
+
+    onMount(loadWorker);
+
+    async function handleEventChange(arg: { event: CalendarEvent }) {
+        if (!dbWorker) {
+            console.error("DB worker is not available.");
+            return;
+        }
+
+        const { id, start, end } = arg.event;
+        const elapsed = new Date(end).getTime() - new Date(start).getTime();
+
+        const toUpdate = `start = ${new Date(
+            start,
+        ).getTime()}, end = ${new Date(end).getTime()}, elapsed = ${elapsed}`;
+        const clause = `id = ${id}`;
+
+        await dbWorker.updateLog(toUpdate, clause);
+        dispatch("eventupdated");
+    }
 
     let options = $state({
         view: "timeGridWeek",
         events: events,
-        eventDidMount: function (arg) {
+        eventDidMount: function (arg: unknown) {
             arg.el.addEventListener("click", () => {
-                event = arg.event;
+                targetEvent = arg.event;
                 modal?.showModal();
             });
         },
+        eventDrop: handleEventChange,
+        eventResize: handleEventChange,
         editable: true,
     });
 </script>
 
 <ContextMenuModal
     bind:modal
-    bind:event
+    bind:event={targetEvent}
     on:eventupdated={() => dispatch("eventupdated")}
 />
 <Calendar plugins={[TimeGrid, Interaction]} {options} />
