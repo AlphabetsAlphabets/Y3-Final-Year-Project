08017fc1912d0da226786e819bdd413bb9135557
Yap Jia Hong <yapjiahong-p@proton.me>
2025-08-23 13:30:24 +0800

    Production version with mobile support. Everything is ok except for summary. The analytics is wrong.

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -5,6 +6,10 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: [vitePreprocess({ script: true })],
+
+  kit: {
+    adapter: adapter({}),
+  },
 };
 
 export default config;
