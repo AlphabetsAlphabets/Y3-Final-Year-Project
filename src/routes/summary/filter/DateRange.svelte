6dfad811f49ebf29f9894b6572cee56fc4a2ab5f
Yap Jia Hong <yapjiahong-p@proton.me>
2025-09-06 21:56:39 +0800

    fix: date range filtering
    The previous implementation would include an entry from a previous date,
    so long as the end date condition was passing. That was stupid lol so I
    decided to combine the two into one, which is what it should've been in
    the first place.

--- src/routes/summary/filter/DateRange.svelte
+++ src/routes/summary/filter/DateRange.svelte
@@ -13,17 +13,13 @@
 
     const filterLogsByDateRange = () => {
         let filtered: Log[] = [];
-        if (startDate) {
-            // timestamp for start of the day before from the current date.
-            const startTimestamp = new Date(startDate).getTime();
-            filtered = logs.filter((log: Log) => log.start >= startTimestamp);
-        }
+        const startTimestamp = new Date(startDate).getTime();
+        const endTimestamp = new Date(endDate).getTime() + 3600 * 24 * 1000;
 
-        if (endDate) {
-            // Add a day from today, filter for everything before it.
-            const endTimestamp = new Date(endDate).getTime() + 3600 * 24 * 1000;
-            filtered = logs.filter((log: Log) => log.end <= endTimestamp);
-        }
+        filtered = logs.filter(
+            (log: Log) =>
+                log.start >= startTimestamp && log.end <= endTimestamp,
+        );
 
         let newPieData = pieChartByActivityOrProject(viewBy, filtered);
 
